8e5023a3e232424868bf79ea24f2467d01792666
praneesh12 <khanna.praneesh@gmail.com>
2021-02-06 10:23:47 -0500

    add folder files

---
+++ weather_forecast/src/engine.py
@@ -0,0 +1,17 @@
+import os
+import datetime
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+import warnings
+warnings.filterwarnings('ignore')
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
